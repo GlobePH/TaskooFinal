3ec334c19d51ee724cf3af211c07c503bc817028
Alexis Pogi <johnlingadx@gmail.com>
2017-07-22 21:06:05 +0800

    Tooltip Hover in each Skills in Customer Page

--- resources/views/homeCustomer.blade.php
+++ resources/views/homeCustomer.blade.php
@@ -51,14 +51,14 @@
     <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 homeSidebar">
       <br /><br />
 
-<a href="#"><div class="row"><img src="{{asset('img/wrench.png')}}" width="30em">&nbsp;&nbsp;Mechanic</div></a>
-<a href="#"><div class="row"><img src="{{asset('img/hammer.png')}}" width="30em">&nbsp;&nbsp;Carpenter</div></a>
-<a href="#"><div class="row"><img src="{{asset('img/bolt.png')}}" width="30em">&nbsp;&nbsp;Electrician</div></a>
-<a href="#"><div class="row"><img src="{{asset('img/coding.png')}}" width="30em">&nbsp;&nbsp;Programmer</div></a>
-<a href="#"><div class="row"><img src="{{asset('img/broom.png')}}" width="30em">&nbsp;&nbsp;House Helper</div></a>
-<a href="#"><div class="row"><img src="{{asset('img/plumbering.png')}}" width="30em">&nbsp;&nbsp;Plumber</div></a>
-<a href="#"><div class="row"><img src="{{asset('img/typewriter.png')}}" width="30em">&nbsp;&nbsp;Encoder</div></a>
-<a href="#"><div class="row"><img src="{{asset('img/tutorial.png')}}" width="30em">&nbsp;&nbsp;Tutor</div></a>
+<a href="#" data-toggle="tooltip" data-placement="right" title="He build walls, roofs, doors, or even your whole house!"><div class="row"><img src="{{asset('img/wrench.png')}}" width="30em">&nbsp;&nbsp;Mechanic</div></a>
+<a href="#" data-toggle="tooltip" data-placement="right" title="Can work with appliances, automobile, and even your bike!"><div class="row"><img src="{{asset('img/hammer.png')}}" width="30em">&nbsp;&nbsp;Carpenter</div></a>
+<a href="#" data-toggle="tooltip" data-placement="right" title="Fixes electrical problems such as wirings, switches or even your phone charger!"><div class="row"><img src="{{asset('img/bolt.png')}}" width="30em">&nbsp;&nbsp;Electrician</div></a>
+<a href="#" data-toggle="tooltip" data-placement="right" title="Can create any program or application depending on your needs!"><div class="row"><img src="{{asset('img/coding.png')}}" width="30em">&nbsp;&nbsp;Programmer</div></a>
+<a href="#" data-toggle="tooltip" data-placement="right" title="Need a help with in your household like cleaning, cooking, or washing of clothes? They're here just for you!"><div class="row"><img src="{{asset('img/broom.png')}}" width="30em">&nbsp;&nbsp;House Helper</div></a>
+<a href="#" data-toggle="tooltip" data-placement="right" title="He is the one doing the dirty work such as clogged toilets, cleaning pipes and more!"><div class="row"><img src="{{asset('img/plumbering.png')}}" width="30em">&nbsp;&nbsp;Plumber</div></a>
+<a href="#" data-toggle="tooltip" data-placement="right" title="They are your type...ist! They can encode your records on different microsoft office applications!"><div class="row"><img src="{{asset('img/typewriter.png')}}" width="30em">&nbsp;&nbsp;Encoder</div></a>
+<a href="#" data-toggle="tooltip" data-placement="right" title="They teach different subject matter based on what you want to learn!"><div class="row"><img src="{{asset('img/tutorial.png')}}" width="30em">&nbsp;&nbsp;Tutor</div></a>
     <!--  <div class="dropdown">
    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Search for Worker
    <span class="caret"></span></button>
@@ -80,6 +80,10 @@
   </div>
 </div>
 
-
+<script>
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+</script>
 </body>
 </html>
