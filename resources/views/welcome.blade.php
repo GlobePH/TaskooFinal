957ec768d3ee6db9caad8a8081636c966b308512
Alexis Pogi <johnlingadx@gmail.com>
2017-07-22 22:37:36 +0800

    Changes in Registration of Customer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -102,7 +102,7 @@
       <div class="container" id="thirdPage">
         <div class="row">
           <p class="mainQuote" align="center">For Workers!</p>
-          <small align="center"><em></em></small>
+          <small><em><center>"Simple work that can make you productive and let you earn money at the same time while seeking a stable job."</center></em></small><br /><br />
           <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
             <p class="mainQuote" align="center">LOGIN</p>
             <form class="form-signin" action="#" method="POST">
@@ -181,34 +181,37 @@
               <form class="form-signin" action="#" method="POST">
                       <div class="form-group">
                         <label for="inputEmail" class="sr-only">Email address</label>
-                        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus value="{{ old('login_email')}}" name="login_email">
+                        <input style="border-radius: 0px;" type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus value="{{ old('login_email')}}" name="login_email">
                       </div>
                       <div class="form-group">
                         <label for="inputPassword" class="sr-only">Password</label>
-                        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required name="password">
+                        <input style="border-radius: 0px;" type="password" id="inputPassword" class="form-control" placeholder="Password" required name="password">
                       </div>
                       <br>
                       {{csrf_field()}}
-                      <button class="btn btn-lg btn-block" type="submit">LOGIN</button>
+                      <button style="border-radius: 0px;background-color:#FFF;border:1px #DDD solid;color:#444;" class="btn btn-lg btn-block" type="submit">LOGIN</button>
                     </form>
+                    <br />
+                    <div class="row">
+                      <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><img src="{{asset('img/analytics.png')}}" width="100em">&nbsp;&nbsp;&nbsp;</div>
+                      <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10"><br />&nbsp;&nbsp;&nbsp;<em>"Pick a worker for your needs and you just help our economy by decreasing those unemployed amigo!"</em></div>
+                    </div>
             </div>
             <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
               <p class="mainQuote" align="center">REGISTER</p>
               <form class="form-signin" action="#" method="POST" enctype="multipart/form-data">
                 <label for="inputEmail" class="sr-only">Email</label>
-                <input type="text" id="inputEmail" class="form-control" placeholder="Email" required autofocus value="{{ old('first_name')}}" name="email"><br>
+                <input type="text" id="inputEmail" class="form-control" style="border-radius: 0px;" placeholder="Email" required autofocus value="{{ old('first_name')}}" name="email"><br>
               <label for="inputFirstName" class="sr-only">First Name</label>
-              <input type="text" id="inputFirstName" class="form-control" placeholder="First Name" required autofocus value="{{ old('first_name')}}" name="first_name"><br>
+              <input type="text" id="inputFirstName" class="form-control"  style="border-radius: 0px;" placeholder="First Name" required autofocus value="{{ old('first_name')}}" name="first_name"><br>
               <label for="inputUsername" class="sr-only">Last Name</label>
-              <input type="text" id="Last Name" class="form-control" placeholder="Last Name" required value="{{ old('last_name')}}" name="last_name"><br>
+              <input type="text" id="Last Name" class="form-control" style="border-radius: 0px;" placeholder="Last Name" required value="{{ old('last_name')}}" name="last_name"><br>
               <label for="inputContactNo" class="sr-only">Contact Number</label>
               <input type="number" id="inputContactNo" class="form-control" style="border-radius: 0px;" placeholder="Contact Number (90********)" maxlength="10" required name="contact_no"><br>
-              {{ csrf_field() }}
-              <!-- <button class="btn btn-lg btn-block btn-primary" type="button" id="btnVerify"><img id="loader" src='{{asset('src/img/index/loader.gif')}}' class="fade" style="margin: -30px; height: 80px;" ><span id="txt">Verify Phone Number</span></button><br> -->
-              <input type="submit" class="btn btn-lg btn-block" name="continue_signup"/>
-           <div id="verify" class="fade">
-              <input type="text" id="vcode" class="form-control" style="border-radius: 0px;" placeholder="Verification Code" required name="verification_code"><br><button class="btn btn-lg btn-block btn-primary"  type="submit">Continue</button>
-            </div>
+                {{ csrf_field() }}
+    <br />
+              <button style="border-radius: 0px;background-color:#FFF;border:1px #DDD solid;color:#444;" class="btn btn-lg btn-block btn-primary"  type="submit">Continue</button>
+
         </form>
             </div>
           </div>
